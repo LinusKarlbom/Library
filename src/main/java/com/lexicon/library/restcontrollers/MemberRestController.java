f06c906edce086e7e9812732aab7a0123dcc3612
LinusKarlbom <linuskar@kth.se>
2018-05-11 11:12:46 +0200

    minor refactoring

--- src/main/java/com/lexicon/library/restcontrollers/MemberRestController.java
+++ src/main/java/com/lexicon/library/restcontrollers/MemberRestController.java
@@ -1,7 +1,5 @@
 package com.lexicon.library.restcontrollers;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
